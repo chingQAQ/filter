d6a61b68eeb94fcde9aca77b12cc96ef7bfef0f1
Aion Chiu <aion.chiu@isobar.com>
2021-10-04 17:44:05 +0800

    feat: first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+﻿import { defineConfig } from 'vite'
+
+import postcssImport from "postcss-import"
+import autoprefixer from 'autoprefixer'
+
+
+export default ({ mode }) => {
+  return defineConfig({
+    css: {
+      postcss: {
+        plugins: [
+          postcssImport,
+          autoprefixer
+        ]
+      }
+    }
+  })
+}
